b27e6c1147a8c6a0871f34f318d35025ac5917e1
rishabh1b <biyanirishabh@gmail.com>
2017-12-15 15:58:06 -0500

    [Iter#3 doxy-1] Adding Doxygen style comments

--- robotarm_grasping/include/robotarm_grasping/SimpleGrasping.h
+++ robotarm_grasping/include/robotarm_grasping/SimpleGrasping.h
@@ -49,40 +49,121 @@
 
 class SimpleGrasping {
  private:
+ 	/**
+ 	 * @brief NodeHandle of the current node
+ 	 */
     ros::NodeHandle nh_;
+    /**
+     * @brief target_pose_1 is the pose to moveit!
+     */
     geometry_msgs::Pose target_pose1;
+    /**
+     * @brief armgroup moveit interface for arm
+     */
     moveit::planning_interface::MoveGroupInterface armgroup;
+    /**
+     * @brief grippegroup moveit interface for gripper
+     */
     moveit::planning_interface::MoveGroupInterface grippergroup;
+    /**
+     * @brief it_ takes care of message to image conversion
+     */
     image_transport::ImageTransport it_;
+    /**
+     * @brief image_sub_ subscribes to image/raw topic
+     */
     image_transport::Subscriber image_sub_;
-
-	// For testing move_group
+    /**
+     * @brief boolean to control the grasping movements
+     */
+	
     bool grasp_running;
+    /**
+     * @brief cv_ptr is the pointer to image as received by it_
+     */
 
     cv_bridge::CvImagePtr cv_ptr;
+    /**
+     * @brief vMng_ is the instance of the library for object detection
+     */
 
 	VisionManager vMng_;
+	/**
+	 * @brief camera_to_robot takes care of the transformation from camera to robot frame
+	 */
 
 	tf::StampedTransform camera_to_robot_;
+	/**
+	 * @brief tf_camera_to_robot is an instance of tf_listener
+	 */
 
 	tf::TransformListener tf_camera_to_robot;
+	/**
+	 * @brief obj_camera_frame, obj_robot_frame are instance of tf::Vector
+	 */
 
 	tf::Vector3 obj_camera_frame, obj_robot_frame;
+	/**
+	 * @brief homePose is StampedPose keeping Home position of arm
+	 */
 
 	geometry_msgs::PoseStamped homePose;
+	/**
+	 * @brief my_plan is an instance of moveit! planning interface
+	 */
 
 	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	/**
+	 * @brief pregrasp_x, pregrasp_y and pregrasp_z is the pregrasp position of arm
+	 */
 
 	float pregrasp_x, pregrasp_y, pregrasp_z;
+	/**
+	 * @brief      attainPosition achieved the given position of the arm
+	 *
+	 * @param[in]  x     x-position of gripping frame	
+	 * @param[in]  y     y-position of gripping frame	
+	 * @param[in]  z     z-position of gripping frame
+	 */
 
 	void attainPosition(float x, float y, float z);
+	/**
+	 * @brief      attainObject tries to get the gripper next to object
+	 */
 	void attainObject();
+	/**
+	 * @brief      grasp executes the grasping action
+	 */
 	void grasp();
+	/**
+	 * @brief      lift attempts to lift the object
+	 */
 	void lift();
 
  public:
+ 	/**
+ 	 * @brief      SimpleGrasping behaviour Constructor
+ 	 *
+ 	 * @param[in]  n_          ros_NodeHandle
+ 	 * @param[in]  pregrasp_x  Desired PregraspingX
+ 	 * @param[in]  pregrasp_y  Desired PregraspingY
+ 	 * @param[in]  pregrasp_z  Desired PregraspingZ
+ 	 * @param[in]  length      The length of table
+ 	 * @param[in]  breadth     The breadth of table
+ 	 */	
 	SimpleGrasping(ros::NodeHandle n_, float pregrasp_x, float pregrasp_y, float pregrasp_z, float length = 1, float breadth = 0.6);
+	/**
+	 * @brief      imageCb is called when a new image is received from the camera
+	 *
+	 * @param[in]  msg   Image received as a message
+	 */
 	void imageCb(const sensor_msgs::ImageConstPtr& msg);
+	/**
+	 * @brief      initiateGrasping initiates the grasping behaviour
+	 */
 	void initiateGrasping();
+	/**
+	 * @brief      Function brings the arm back to home configuration
+	 */
 	void goHome();
 };
--- robotarm_grasping/include/robotarm_grasping/VisionManager.h
+++ robotarm_grasping/include/robotarm_grasping/VisionManager.h
@@ -43,21 +43,79 @@
 
 class VisionManager {
   public :
+  /**
+   * @brief      VisionManager Constructor
+   *
+   * @param[in]  length   The length of the table
+   * @param[in]  breadth  The breadth of the table
+   */
 	VisionManager(float length, float breadth);
+	/**
+	 * @brief      Gets the 2d location of object in camera frame
+	 *
+	 * @param[in]  img   The image
+	 * @param      x     x postion of the object
+	 * @param      y     y position of the object
+	 */
 	void get2DLocation(cv::Mat img, float& x, float& y);
 
  private:
+ 	/**
+ 	 * @brief      detect2DObject processes the image to isolate object
+ 	 *
+ 	 * @param      pixel_x  postion of the object in x-pixels
+ 	 * @param      pixel_y  positino of the object in y-pixels
+ 	 */
 	void detect2DObject(float& pixel_x, float& pixel_y);
+	/**
+	 * @brief      convertToMM converts pixel measurement to metric
+	 *
+	 * @param      pixel_mm_x  The pixel millimeters per x
+	 * @param      pixel_mm_y  The pixel millimeters per y
+	 */
 	void convertToMM(float& pixel_mm_x, float& pixel_mm_y);
+	/**
+	 * @brief      detectTable isolates the table to get pixel to metric conversion
+	 */
 	void detectTable();
+	/**
+	 * @brief pixels per mm in x for the camera
+	 */
 	float pixels_permm_x;
+	/**
+	 * @brief pixels per mm in y for the camera
+	 */
 	float pixels_permm_y;
+	/**
+	 * @brief curr_pixel_centre_x is the object location in x
+	 */
 	float curr_pixel_centre_x;
+	/**
+	 * @brief curr_pixel_centre_y is the object location in y
+	 */
 	float curr_pixel_centre_y;
+	/**
+	 * @brief table length in meters
+	 */
 	float table_length;
+	/**
+	 * @brief table breadth in meters
+	 */
 	float table_breadth;
+	/**
+	 * @brief centre of the image in pixels x
+	 */
 	float img_centre_x_;
+	/**
+	 * @brief centre of the image in pixels y
+	 */
 	float img_centre_y_;
+	/**
+	 * @brief boolean to prevent detectTable running every time
+	 */
 	bool pixel_size_identified;
+	/**
+	 * @brief curr_img is the image currently being processed
+	 */
 	cv::Mat curr_img;
 };
