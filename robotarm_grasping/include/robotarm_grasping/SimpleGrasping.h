489bd2769a51bdb0fe7081e103d5468f73b02069
rishabh1b <biyanirishabh@gmail.com>
2017-12-15 15:25:06 -0500

    [Iter#3 lint] adding cpplint output

--- robotarm_grasping/include/robotarm_grasping/SimpleGrasping.h
+++ robotarm_grasping/include/robotarm_grasping/SimpleGrasping.h
@@ -1,15 +1,51 @@
+#pragma once
+/**
+* @file SimpleGrasping.h Class Declaration of Simple Grasping class
+* @author rishabh1b(Rishabh Biyani)
+* @copyright BSD 3-Clause License (c) Rishabh Biyani 2017
+*/
+
+// BSD 3-Clause License
+
+// Copyright (c) 2017, Rishabh Biyani
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and / or other materials provided with the distribution.
+
+// * Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
-#include "geometry_msgs/PoseStamped.h"
+#include <geometry_msgs/PoseStamped.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 #include <cv_bridge/cv_bridge.h>
-#include "robotarm_grasping/VisionManager.h"
 #include <tf/transform_listener.h>
+#include "robotarm_grasping/VisionManager.h"
 
 class SimpleGrasping {
  private:
@@ -44,7 +80,7 @@ class SimpleGrasping {
 	void grasp();
 	void lift();
 
-  public:
+ public:
 	SimpleGrasping(ros::NodeHandle n_, float pregrasp_x, float pregrasp_y, float pregrasp_z, float length = 1, float breadth = 0.6);
 	void imageCb(const sensor_msgs::ImageConstPtr& msg);
 	void initiateGrasping();
--- robotarm_grasping/src/VisionManager.cpp
+++ robotarm_grasping/src/VisionManager.cpp
@@ -1,3 +1,42 @@
+// BSD 3-Clause License
+
+// Copyright (c) 2017, Rishabh Biyani
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and / or other materials provided with the distribution.
+
+// * Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//         SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+/**
+* @file VisionManger.cpp Class Implementations to detect the object
+* and return the co-ordinates in the camera frame
+* @author rishabh1b(Rishabh Biyani)
+* @copyright BSD 3-Clause License (c) Rishabh Biyani 2017
+*/
+
+
 #include "robotarm_grasping/VisionManager.h"
 
 VisionManager::VisionManager(float length, float breadth) {
@@ -34,7 +73,7 @@ void VisionManager::detectTable() {
 			int editValue = binaryImage.at<uchar>(i, j);
 			int editValue2 = gray_image_green.at<uchar>(i, j);
 
-			if ((editValue > 34) && (editValue < 50) && (editValue2 > 18) && (editValue2 < 30)) { //check whether value is within range.
+			if ((editValue > 34) && (editValue < 50) && (editValue2 > 18) && (editValue2 < 30)) { // check whether value is within range.
 				binaryImage.at<uchar>(i, j) = 255;
 			} else {
 				binaryImage.at<uchar>(i, j) = 0;
@@ -67,15 +106,14 @@ void VisionManager::detectTable() {
 	std::vector<std::vector<cv::Point> > contours;
 	std::vector<cv::Vec4i> hierarchy;
 
-	cv::findContours( binaryImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-	for ( int i = 0; i < contours.size(); i++ ) {
-		cv::Scalar color = cv::Scalar( 255, 0, 0);
-		cv::drawContours( image, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+	cv::findContours(binaryImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+	for (int i = 0; i < contours.size(); i++) {
+		cv::Scalar color = cv::Scalar(255, 0, 0);
+		cv::drawContours(image, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
 	}
 
 	// cv::namedWindow("Table Detection", cv::WINDOW_AUTOSIZE);
 	// cv::imshow("Table Detection", image);
-
 }
 
 void VisionManager::detect2DObject(float& pixel_x, float& pixel_y) {
@@ -112,10 +150,10 @@ void VisionManager::detect2DObject(float& pixel_x, float& pixel_y) {
 	std::vector<std::vector<cv::Point> > contours;
 	std::vector<cv::Vec4i> hierarchy;
 
-	cv::findContours( binaryImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+	cv::findContours(binaryImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 	for ( int i = 0; i < contours.size(); i++ ) {
-		cv::Scalar color = cv::Scalar( 255, 0, 0);
-		cv::drawContours( image, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+		cv::Scalar color = cv::Scalar(255, 0, 0);
+		cv::drawContours(image, contours, i, color, 1, 8, hierarchy, 0, cv::Point());
 	}
 
 	// cv::namedWindow("Centre point", cv::WINDOW_AUTOSIZE);
@@ -124,7 +162,6 @@ void VisionManager::detect2DObject(float& pixel_x, float& pixel_y) {
 
 void VisionManager::convertToMM(float& x, float& y) {
 	// Convert from pixel to world co-ordinates in the camera frame
-
 	x = (x - img_centre_x_) / pixels_permm_x;
 	y = (y - img_centre_y_) / pixels_permm_y;
 }
